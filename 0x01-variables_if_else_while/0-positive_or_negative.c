787305e1f9d1fc6b7aef0850fa955ae17efb3b54
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-09 12:56:41 -0700

    random number generator

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /*more headers goes there*/
@@ -11,9 +12,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/*your code goes there*/
 	if (n > 0)
+	{
 		printf("%d is positive\n",n);
+	}
 	if (n < 0)
+	{
 		printf("%d is negative\n",n);
+	}
 	if (n == 0)
+	{
 		printf("%d is zero\n",n);
+	}
 }
