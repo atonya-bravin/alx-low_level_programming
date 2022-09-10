8a805f078ded115f53ce639905bde066ef0c68e9
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-10 13:52:07 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - Entry Point
+ *
+ * Description: 'this program prints all the
+ * alphabetical letters in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	int letter_count =0;
+	int letter_count = 0;
+
 	for (letter_count = 97; letter_count < 123; letter_count++)
 	{
 		putchar(letter_count);
