65c1f92cf2a235f8091549f6d07f593b6bd59306
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-10 14:18:55 -0700

    print alphabets in both cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'this program print alphabets
+ * both in upper and lower case'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int first_letter_number = 0;
+	int last_letter_number = 123;
+
+	for(first_letter_number = 97; first_letter_number < last_letter_number; first_letter_number++)
+	{
+		putchar(first_letter_number);
+		if(first_letter_number == 122)
+		{
+			first_letter_number = 64;
+			last_letter_number = 91;	
+		}
+	}
+	putchar('\n');
+	return (0);
+}
