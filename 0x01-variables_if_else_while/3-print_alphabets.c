f175596ecf503f41f2147739e65ac28b2b5d051b
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-10 14:23:20 -0700

    print alphabets in both cases

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,15 +14,15 @@ int main(void)
 	int first_letter_number = 0;
 	int last_letter_number = 123;
 
-	for(first_letter_number = 97;
+	for (first_letter_number = 97;
 			first_letter_number < last_letter_number;
 			first_letter_number++)
 	{
 		putchar(first_letter_number);
-		if(first_letter_number == 122)
+		if (first_letter_number == 122)
 		{
 			first_letter_number = 64;
-			last_letter_number = 91;	
+			last_letter_number = 91;
 		}
 	}
 	putchar('\n');
