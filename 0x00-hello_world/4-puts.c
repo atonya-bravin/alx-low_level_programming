86c8ecc21cc9fda891000ed0b7248bd16e896d02
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-11 19:35:44 -0700

    prints text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point 
+ *
+ * Description: 'This program prints text'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
