813d52641ca9ec4444c227df8606f318fa6cab1e
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-12 19:43:56 -0700

    generate intel assembly code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,11 +16,11 @@ int main(void)
 	long longInteger;
 	long long doubleLongInteger;
 
-	printf("Size of a char: %ld byte(s)", sizeOf(character));
-	printf("Size of a int: %ld byte(s)", sizeOf(integer));
-	printf("Size of a long int: %ld byte(s)", sizeOf(longInteger));
-	printf("Size of a long long int: %ld byte(s)", sizeOf(doubleLongInteger));
-	printf("Size of a float: %ld byte(s)", sizeOf(floating));
+	printf("Size of a char: %d byte(s)", sizeOf(character));
+	printf("Size of a int: %d byte(s)", sizeOf(integer));
+	printf("Size of a long int: %d byte(s)", sizeOf(longInteger));
+	printf("Size of a long long int: %d byte(s)", sizeOf(doubleLongInteger));
+	printf("Size of a float: %d byte(s)", sizeOf(floating));
 
 	return (0);
 }
