a33b5e256e8288a1d8c46d6e9a21e2232d165478
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-14 14:23:00 -0700

    find sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - addition function
+ *
+ * Description: takes in two numbers then retuns their sum
+ *
+ * Return: Always sum
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
