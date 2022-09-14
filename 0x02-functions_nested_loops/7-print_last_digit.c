2857cd0218975c2b4bea2635b24c57e79a3bf424
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-14 14:09:41 -0700

    last digit printer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,7 +13,12 @@
 
 int print_last_digit(int c)
 {
-	int last_digit = _abs(c) % 10;
+	int last_digit;
+
+	if (c > 0)
+		last_digit = c % 10;
+	else
+		last_digit = (c + (c * -2)) % 10;
 
 	_putchar(last_digit + '0');
 	return (last_digit);
