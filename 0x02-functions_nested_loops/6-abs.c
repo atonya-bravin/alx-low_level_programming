7c2d5a204760e42117f32a978c5572feaa7e7750
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-14 13:33:28 -0700

    compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - compute absolute
+ *
+ * Description: "gets a number and computes the absolute"
+ *
+ * @c: the number to compute absolute
+ *
+ * Return: Always absolute value
+ */
+
+int _abs(int c)
+{
+	return (((c) + (c * -2)));
+}
