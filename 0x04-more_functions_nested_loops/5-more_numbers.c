2b9e8cca0745ab23da2d856f672367af7c151911
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-16 02:01:23 -0700

    prints 1 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14
+ *
+ * Description: prints 0 to 14 followed by new line
+ *
+ * Return: Always Nothing
+ */
+
+void more_numbers(void)
+{
+	int printCounter;
+	int numberCounter;
+
+	for (printCounter = 0; printCounter < 10; printCounter++)
+	{
+		for (numberCounter = 0; numberCounter < 15; numberCounter++)
+		{
+			if (numberCounter < 10)
+			{
+				_putchar((numberCounter % 10) + '0');
+			}
+			else
+			{
+				_putchar(((numberCounter - (numberCounter % 10)) / 10) + '0');
+				_putchar((numberCounter % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
