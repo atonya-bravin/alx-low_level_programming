d83667a949399d9b197e50a89cf03717e97cc12d
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-15 23:37:49 -0700

    number multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiplication function
+ *
+ * Description: multiplies two numbers and returns the result
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: Always result
+ */
+int mul(int a, int b)
+{
+	int first_number = a;
+	int second_number = b;
+
+	return (first_number * second_number);
+}
