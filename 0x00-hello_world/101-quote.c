a1fd21387e28c3f01bd49c3014b04ba6ce75da0b
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-17 20:28:13 -0700

    stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints to the stderr
+ *
+ * Description: "prints a string
+ * followed by a new line"
+ *
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	fprintf(stderr
+			, "%s\n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (1);
+}
