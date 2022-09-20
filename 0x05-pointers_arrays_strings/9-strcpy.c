00f979c03b1849e8083ea5d2067e4fd991fd243f
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-20 16:10:09 -0700

    copy characters

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string
+ *
+ * Description: takes in a string and copies it icluding null byte
+ *
+ * @dest: destination pointer
+ * @src: source pointer
+ *
+ * Return: Always destination pointer
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int string_length;
+	int character_counter;
+
+	for (character_counter = 0; src[character_counter] !=
+			'\0'; character_counter++)
+	{
+		string_length++;
+	}
+
+	for (character_counter = 0; character_counter <
+			string_length; character_counter++)
+		dest[character_counter] = src[character_counter];
+
+	return (dest);
+}
