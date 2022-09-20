f3ae2a9d1390da52568bbe17ced361ad7e543b13
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-20 12:28:42 -0700

    prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *puts_half - prints a string to stdout
+ *
+ * Description: takes in a string and prints characters to screen one by one
+ *
+ * @str: string pointer
+ *
+ * Return: Always Nothing
+ */
+
+void puts_half(char *str)
+{
+	int character_counter = 0;
+	int string_length = 0;
+	int half_string_character_counter = 0;
+
+	while (str[character_counter] != '\0')
+		character_counter++;
+
+	string_length = character_counter--;
+
+	half_string_character_counter = string_length / 2;
+
+	while (half_string_character_counter <= string_length)
+	{
+		_putchar(str[half_string_character_counter]);
+		half_string_character_counter++;
+	}
+	_putchar('\n');
+}
