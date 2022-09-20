61222ac2934d58e65eb58e4330bb43fe83d3c4bb
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-19 18:52:20 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets passed value to 98
+ *
+ * Description - this function accepts a parameter and changes the value to 98
+ *
+ *@n: parameter to use in changing the value of our variable
+ *
+ * Return: Always Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef RESET_TO_98
+#define RESET_TO_98
+
+void reset_to_98(int *n);
+int _putchar(char c);
+
+#endif
