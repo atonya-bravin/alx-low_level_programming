a3a75aaa9e1d34dca0937b31c4756b3bea7a0d07
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-19 19:44:28 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ *
+ * Description: takes in a string and prints characters to screen one by one
+ *
+ * @str: string pointer
+ *
+ * Return: Always Nothing
+ */
+
+void _puts(char *str)
+{
+	int character_counter = 0;
+
+	while (str[character_counter] != '\0')
+	{
+		_putchar(str[character_counter]);
+		character_counter++;
+	}
+}
