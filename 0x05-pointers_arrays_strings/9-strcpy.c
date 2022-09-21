a547baf589a5924b804a6d4c740bd2fd7fd6ed5a
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-20 17:18:45 -0700

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -25,7 +25,7 @@ char *_strcpy(char *dest, char *src)
 		string_length++;
 	}
 	string_length++;
-	for (character_counter = 0; character_counter  <=
+	for (character_counter = 0; character_counter  <
 			string_length; character_counter++)
 	{
 		tmp = src[character_counter];
