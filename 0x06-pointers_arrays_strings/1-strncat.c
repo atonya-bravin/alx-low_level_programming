2ceefa8d094384b9810d7777cdc4da9acff00c4f
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-22 14:51:10 -0700

    string concatination with number of characters to concatinate

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -20,15 +20,23 @@ char *_strncat(char *dest, char *src, int n)
 	int character_counter;
 	int proposed_src_string_length = n;
 	int destination_string_length = 0;
-
+	/**
+	int source_string_length;
+	*/
 	for (character_counter = 0;
 			dest[character_counter] != '\0';
 			character_counter++)
 		destination_string_length++;
-
+	/**
+	 * 
+	 for (character_counter = 0;
+	 src[character_counter] != '\0';
+	 character_counter++)
+	 source_string_length++;
+	 */
 	for (character_counter = 0;
-			(character_counter < proposed_src_string_length)
-			&& (src[character_counter] != '\0');
+			(character_counter < proposed_src_string_length
+			 && src[character_counter] != '\0');
 			character_counter++)
 		dest[destination_string_length + character_counter] = src[character_counter];
 
