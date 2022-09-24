0944753d61705fd4c227deef0e08b022fb3b782e
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-23 19:10:29 -0700

    concatinates one string to another

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strncat(char *dest, char *src, int n) - concatenates two strings
+ * _strncat - concatenates two strings
  *
- * Description: takes two stings dest and src and one integer(n)
+ * Description: "takes two stings dest and src and one integer(n)
  * it uses only n characters of the src string
- * src does not need to be null-teminated
+ * src does not need to be null-teminated"
  *
  * @src : source string pointer
  * @dest: destination string pointer
@@ -19,28 +19,19 @@ char *_strncat(char *dest, char *src, int n)
 	int character_counter;
 	int proposed_src_string_length = n;
 	int destination_string_length = 0;
-	/**
-	 * int source_string_length;
-	 */
+
 	for (character_counter = 0;
 			dest[character_counter] != '\0';
 			character_counter++)
 		destination_string_length++;
-	/**
-	 *
-	 * for (character_counter = 0;
-	 * src[character_counter] != '\0';
-	 * character_counter++)
-	 * source_string_length++;
-	 */
+
 	for (character_counter = 0;
 			(character_counter < proposed_src_string_length)
 			&& (src[character_counter] != '\0');
 			character_counter++)
 		dest[destination_string_length + character_counter] = src[character_counter];
 
-	if (destination_string_length < n)
-		dest[destination_string_length + n] = '\0';
+	dest[destination_string_length + character_counter] = '\0';
 
 	return (dest);
 }
