e42df4d0b353132a6919c8a3ac5cbc56ca38bdbc
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-23 18:58:03 -0700

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,12 +18,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int character_counter;
 	int source_string_length = 0;
-	int destination_string_length = 0;
-
-	for (character_counter = 0;
-			dest[character_counter] != '\0';
-			character_counter++)
-		destination_string_length++;
+	int proposed_string_length = n;
 
 	for (character_counter = 0;
 			src[character_counter] != '\0';
@@ -31,10 +26,13 @@ char *_strncpy(char *dest, char *src, int n)
 		source_string_length++;
 
 	for (character_counter = 0;
-			character_counter < n;
+			character_counter < proposed_string_length
+			&& src[character_counter] != '\0';
 			character_counter++)
-		dest[destination_string_length + character_counter] = src[character_counter];
-	
-	putchar('\n');
+		dest[character_counter] = src[character_counter];
+
+	for ( ; source_string_length < n; source_string_length++)
+		dest[source_string_length] = '\0';
+
 	return (dest);
 }
