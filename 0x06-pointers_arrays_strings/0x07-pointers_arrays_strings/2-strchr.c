71a43208aad7a206a28b0d276ac15669a9df5dde
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-26 14:09:31 -0700

    character scanner

---
+++ 0x06-pointers_arrays_strings/0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locates a character in a string
+ *
+ * Description: takes in a string and a character to find
+ * finds the first occurence of our target character
+ * returns the pointer to the first occurence of the target character
+ *
+ * @s: string to search character from
+ * @c: character to search for
+ *
+ * Return: Always character poiter
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *target_string = s;
+	char target_character = c;
+	char *returning_pointer;
+	int character_counter;
+
+	for (character_counter = 0;
+			target_string[character_counter] != '\0'; character_counter++)
+	{
+		if (target_string[character_counter] == target_character)
+		{
+			returning_pointer = &target_string[character_counter];
+			break;
+		}
+		if (target_string[character_counter] != target_character &&
+				(target_string[character_counter + 1] == '\0'))
+			returning_pointer = NULL;
+	}
+	return (returning_pointer);
+}
---
+++ 0x06-pointers_arrays_strings/0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef EVEN_MORE_POINTERS
+#define EVEN_MORE_POINTERS
+
+char *_strchr(char *s, char c);
+
+#endif
