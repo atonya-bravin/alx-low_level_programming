9c749ef117e8d44cbed921008b80d172bdff4001
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-30 12:22:10 -0700

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of a program followe by a new line
+ *
+ * Description: "takes in arguments from the command line and prints the first argument"
+ *
+ * @argc: takes in the number of arguments passed in from command line
+ * @argv: agument array holding arguments from command line
+ *
+ * Return: Always Nothing
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
