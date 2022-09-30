59502f96db0d8ed063f5259d7febea7ac06e3e57
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-09-30 15:22:37 -0700

    calculates the number of coins in change of an amount

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ *
+ * Description: "prints Error if number of the arguments
+ * passed is not exactly 1
+ * if number passed as the argument is negatice, print 0
+ * change is given in 25(s), 10(s), 5(s), 2(s), and 1(s)"
+ *
+ * @argc: number of arguments passed through the command line
+ * @argv: the arguments array pointer
+ *
+ * Return: Always 0(success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int remaining_cents;
+	int number_of_change_cents = 0;
+
+	if (argc == 2)
+	{
+		remaining_cents = atoi(argv[1]);
+		while (remaining_cents > 0)
+		{
+			if ((remaining_cents % 25) == 0)
+			{
+				remaining_cents -= 25;
+				number_of_change_cents++;
+			}
+			else if ((remaining_cents % 10) == 0)
+			{
+				remaining_cents -= 10;
+				number_of_change_cents++;
+			}
+			else if ((remaining_cents % 5) == 0)
+			{
+				remaining_cents -= 5;
+				number_of_change_cents++;
+			}
+			else if ((remaining_cents % 2) == 0)
+			{
+				remaining_cents -= 2;
+				number_of_change_cents++;
+			}
+			else if ((remaining_cents % 1) == 0)
+			{
+				remaining_cents -= 1;
+				number_of_change_cents++;
+			}
+		}
+		printf("%d\n", number_of_change_cents);
+	}
+	else
+		printf("Error\n");
+
+	return (0);
+}
