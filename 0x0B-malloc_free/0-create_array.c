2b76d4622a7f0a75a92b09cf64f906c9ef2bc083
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-10-03 19:58:54 -0700

    char size allocator

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars and initializes with specific char.
+ *
+ * @size: the to use in intialization
+ * @c: the character to intialize in memory
+ *
+ * Return: pointer to array (success) NULL (fail)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int byte_size = sizeof(char) * size;
+	char *character_pointer = (malloc(byte_size));
+	unsigned int bytes_counter = 0;
+
+	if (size == 0)
+		return ('\0');
+
+	else
+	{
+		for (; bytes_counter < size; bytes_counter++)
+			character_pointer[bytes_counter] = c;
+	}
+	return (&character_pointer[0]);
+}
