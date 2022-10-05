02acab002d1482339cd861cdefb579933886ce85
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-10-05 12:46:19 -0700

    dynumic multidimentional arrays

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to 2 dimensional array of integers
+ *
+ * @width: the width of the 2 dimensional array
+ * @height: the height of the 2 dimesional array
+ *
+ * Return: Always NULL (fail)
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int rows_counter = 0;
+	int column_counter = 0;
+	int **grid = malloc(sizeof(int *) * width);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (column_counter = 0; column_counter < width; column_counter++)
+	{
+		grid[column_counter] = malloc(sizeof(int) * height);
+		if (grid[column_counter] == NULL)
+			return (NULL);
+	}
+
+	for (column_counter = 0; column_counter < width; column_counter++)
+	{
+		for (rows_counter = 0; rows_counter < height; rows_counter++)
+			grid[column_counter][rows_counter] = 0;
+	}
+	return (grid);
+}
