102cf0ab0699fe78433d1e4e9d9b2c397937b940
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-10-06 13:47:39 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * Description: "allocates memory using the malloc function
+ * returns the pointer to the allocated memory"
+ *
+ * @b: the number of bytes to allocate
+ *
+ * Return: memory allocated pointer (success)
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *space_pointer;
+	
+	space_pointer = malloc(b);
+	
+	if (space_pointer == 0)
+		exit(98);
+	else
+		return(space_pointer);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef MORE_MALLOC
+#define MORE_MALLOC
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+#endif
