6f9c9d97ca02bcb5c2eadf4eab0511006ece78f4
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-10-09 12:11:16 -0700

    print type sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,11 +16,11 @@ int main(void)
 	long longInteger;
 	long long doubleLongInteger;
 
-	printf("Size of a char: %d byte(s)", sizeOf(character));
-	printf("Size of a int: %d byte(s)", sizeOf(integer));
-	printf("Size of a long int: %d byte(s)", sizeOf(longInteger));
-	printf("Size of a long long int: %d byte(s)", sizeOf(doubleLongInteger));
-	printf("Size of a float: %d byte(s)", sizeOf(floating));
+	printf("Size of a char: %lu byte(s)", sizeof(character));
+	printf("Size of a int: %lu byte(s)", sizeof(integer));
+	printf("Size of a long int: %lu byte(s)", sizeof(longInteger));
+	printf("Size of a long long int: %lu byte(s)", sizeof(doubleLongInteger));
+	printf("Size of a float: %lu byte(s)", sizeof(floating));
 
 	return (0);
 }
