bc27c315d0128bc543fe73b43c18d605867f1924
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-10-09 12:43:34 -0700

    types size checker

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,11 +16,11 @@ int main(void)
 	long longInteger;
 	long long doubleLongInteger;
 
-	printf("Size of a char: %lu byte(s)", sizeof(character));
-	printf("Size of a int: %lu byte(s)", sizeof(integer));
-	printf("Size of a long int: %lu byte(s)", sizeof(longInteger));
-	printf("Size of a long long int: %lu byte(s)", sizeof(doubleLongInteger));
-	printf("Size of a float: %lu byte(s)", sizeof(floating));
+	printf("Size of a char: %lu byte(s)\n", sizeof(character));
+	printf("Size of a int: %lu byte(s)\n", sizeof(integer));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(longInteger));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(doubleLongInteger));
+	printf("Size of a float: %lu byte(s)\n", sizeof(floating));
 
 	return (0);
 }
