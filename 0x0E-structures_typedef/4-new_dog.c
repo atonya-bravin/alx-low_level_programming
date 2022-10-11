ad084d2e78ec174698cbb47599fcf744c2cb3429
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-10-11 10:54:47 -0700

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,7 +25,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	new_doggy->name = malloc((sizeof(name)) + 1);
+	if (new_doggy->name == NULL)
+	{
+		free(new_doggy);
+		return (NULL);
+	}
 	new_doggy->owner = malloc((sizeof(owner)) + 1);
+	if (new_doggy->owner == NULL)
+	{
+		free(new_doggy);
+		return (NULL);
+	}
 
 	for (; name[character_counter] != '\0'; character_counter++)
 		new_doggy->name[character_counter] = name[character_counter];
