def255594a2b71c41dbdabcd4ef8405c833a5a8d
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-10-11 05:55:52 -0700

    structure initit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ *
+ * @d: the dog structure
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the owner name
+ *
+ * Return: Always Nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+}
