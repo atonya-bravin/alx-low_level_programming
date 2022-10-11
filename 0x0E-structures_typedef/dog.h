b148aed8322aef57fdbc7ed60ad5812cd777d5b8
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-10-11 05:34:58 -0700

    dog structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef POPPY
 #define POPPY
 
+/**
+ * struct dog - structure that corries all dog's intormation
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: name of the owner
+ */
 struct dog
 {
 	char *name;
