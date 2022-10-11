5deaa94cb24af097e3a0a33ba85f13f6b0096403
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-10-11 06:40:03 -0700

    print structure

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,9 +15,9 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("Name: (nil)");
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		printf("Owner: (nil)");
+		d->owner = "(nil)";
 	if (d)
 	{
 		printf("Name: %s\n", d->name);
