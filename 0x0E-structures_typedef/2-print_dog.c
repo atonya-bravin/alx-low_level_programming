832f279fbf2333fcf28f74b3442bd0ebc334bcb6
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-10-11 06:23:27 -0700

    print structure

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@
 
 void print_dog(struct dog *d)
 {
-	if (d->name == NULL || d->age == '0' || d->owner == NULL)
+	if (d->name == NULL || d->age == 0.0 || d->owner == NULL)
 		printf("Name: (nil)");
 	if (d)
 	{
