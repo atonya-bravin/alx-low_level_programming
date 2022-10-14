2875bdd791b085018d060941590363405eec0363
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-10-14 09:17:40 -0700

    print everything passed as parameter

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,7 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdbool.h>
 
 /**
  * print_all - prints anything
@@ -13,36 +14,49 @@
 void print_all(const char * const format, ...)
 {
 	int char_counter = 0;
+	bool break_out = false;
 
 	va_list argument_list;
 
 	va_start(argument_list, format);
 
-	while (format[char_counter] != '\0')
+	while(format != NULL && (!break_out))
 	{
-		if (format[char_counter] == 'c' || format[char_counter] == 'i' ||
-				format[char_counter] == 'f' || format[char_counter] == 's')
+		while (format[char_counter] != '\0')
 		{
-			switch (format[char_counter])
+			if (format[char_counter] == 'c' || format[char_counter] == 'i' ||
+					format[char_counter] == 'f' || format[char_counter] == 's')
 			{
-				case 'c':
-					printf("%c", va_arg(argument_list, int));
-					break;
-				case 'i':
-					printf("%d", va_arg(argument_list, int));
-					break;
-				case 'f':
-					printf("%f", va_arg(argument_list, double));
-					break;
-				default:
-					printf("%s", va_arg(argument_list, char*));
-					break;
-			}
-			if (format[char_counter + 1] != '\0')
-				printf(", ");
-		}
-		char_counter++;
+				switch (format[char_counter])
+				{
+					case 'c':
+						printf("%c", va_arg(argument_list, int));
+						break;
+					case 'i':
+						printf("%d", va_arg(argument_list, int));
+						break;
+					case 'f':
+						printf("%f", va_arg(argument_list, double));
+						break;
+					default:
+						printf("%s", va_arg(argument_list, char*));
+						break;
+				}
+				if (format[char_counter + 1] != '\0')
+					printf(", ");
+				switch (format[char_counter + 1])
+				{
+					case '\0':
+						break_out = true;
+						break;
+					default:
+						break;
+				}
 
+			}
+			char_counter++;
+
+		}
 	}
 	va_end(argument_list);
 	printf("\n");
