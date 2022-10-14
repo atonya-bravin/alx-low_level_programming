20d7955beff7b02116220c0d546431f6254daaa2
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-10-14 08:36:53 -0700

    print everything passed as parameter

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - prints anything
+ *
+ * @format: format string to use as guide to what type to expect
+ *
+ * Return: Always Nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	int char_counter = 0;
+
+	va_list argument_list;
+
+	va_start(argument_list, format);
+
+	while (format[char_counter] != '\0')
+	{
+		if (format[char_counter] == 'c' || format[char_counter] == 'i' ||
+				format[char_counter] == 'f' || format[char_counter] == 's')
+		{
+			switch (format[char_counter])
+			{
+				case 'c':
+					printf("%c", va_arg(argument_list, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(argument_list, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(argument_list, double));
+					break;
+				case 's':
+					printf("%s", va_arg(argument_list, char*));
+					break;
+			}
+			if (format[char_counter + 1] != '\0')
+				printf(", ");
+		}
+		char_counter++;
+
+	}
+	va_end(argument_list);
+	printf("\n");
+}
