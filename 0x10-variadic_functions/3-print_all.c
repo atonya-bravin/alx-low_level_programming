9286770e2df47cff6c41a690222e22c6c60ad627
Bravin Imbarabara <bravinatonya.brani@gmail.com>
2022-10-14 14:34:34 -0700

    print all arguments

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,50 +13,34 @@
 
 void print_all(const char * const format, ...)
 {
-	int char_counter = 0;
-	bool break_out = false;
-
+	int c = 0;
 	va_list argument_list;
 
 	va_start(argument_list, format);
-
-	while(format != NULL && (!break_out))
+	while (format != NULL && format[c] != '\0')
 	{
-		while (format[char_counter] != '\0')
+		if (format[c] == 'c' || format[c] == 'i' ||
+				format[c] == 'f' || format[c] == 's')
 		{
-			if (format[char_counter] == 'c' || format[char_counter] == 'i' ||
-					format[char_counter] == 'f' || format[char_counter] == 's')
+			switch (format[c])
 			{
-				switch (format[char_counter])
-				{
-					case 'c':
-						printf("%c", va_arg(argument_list, int));
-						break;
-					case 'i':
-						printf("%d", va_arg(argument_list, int));
-						break;
-					case 'f':
-						printf("%f", va_arg(argument_list, double));
-						break;
-					default:
-						printf("%s", va_arg(argument_list, char*));
-						break;
-				}
-				if (format[char_counter + 1] != '\0')
-					printf(", ");
-				switch (format[char_counter + 1])
-				{
-					case '\0':
-						break_out = true;
-						break;
-					default:
-						break;
-				}
-
+				case 'c':
+					printf("%c", va_arg(argument_list, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(argument_list, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(argument_list, double));
+					break;
+				default:
+					printf("%s", va_arg(argument_list, char*));
+					break;
 			}
-			char_counter++;
-
+			if (format[c + 1] != '\0')
+				printf(", ");
 		}
+		c++;
 	}
 	va_end(argument_list);
 	printf("\n");
