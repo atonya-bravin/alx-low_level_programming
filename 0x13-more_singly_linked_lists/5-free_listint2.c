4c1441853b9b322f17023d6208bd50a19aa2e102
bravin-atonya <bravinatonya.brani@gmail.com>
2022-10-26 19:21:18 -0700

    free a linked list and assign NULL to its head

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees a linked list and assigns NULL to its head
+ *
+ * @head: the head of the linked list
+ *
+ * Return: Always Nothing
+ *
+ */
+
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current_node = *head;
+	listint_t *auxilarryNode;
+
+	while (current_node != NULL)
+	{
+		auxilarryNode = current_node;
+		current_node = current_node->next;
+		free(auxilarryNode);
+	}
+	current_node = NULL;
+}
