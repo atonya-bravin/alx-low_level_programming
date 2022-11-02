7d1e620dce1bdc19a0391ef474fbcd2700d88e30
bravin-atonya <bravinatonya.brani@gmail.com>
2022-11-02 12:17:39 -0700

    handling of the read error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,6 +25,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	number_of_read_char = read(access_granted, character_holder, letters);
+	
+	if (number_of_read_char == -1)
+		return (0);
 
 	printf("%s", character_holder);
 	return (number_of_read_char);
