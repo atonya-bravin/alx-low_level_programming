b91cadfd40c835f819d65f599b00d489abad67a3
bravin-atonya <bravinatonya.brani@gmail.com>
2022-11-04 13:39:37 -0700

    truncate if file exists

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -43,7 +43,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		access_granted = open(filename, O_TRUNC, 00600);
+		access_granted = open(filename, O_TRUNC);
 
 	else
 	{
