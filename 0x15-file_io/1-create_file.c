09791eb6b1a958bbdf58b326de6a4eaf2d679fe5
bravin-atonya <bravinatonya.brani@gmail.com>
2022-11-04 12:46:53 -0700

    correct betty styling

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -42,7 +42,7 @@ int create_file(const char *filename, char *text_content)
 	if ((filename == NULL) | (text_content == NULL))
 		return (-1);
 
-	access_granted = open(filename, O_CREAT|O_RDWR|O_APPEND, 00600);
+	access_granted = open(filename, O_CREAT | O_RDWR | O_APPEND, 00600);
 
 	if (access_granted == -1)
 	{
