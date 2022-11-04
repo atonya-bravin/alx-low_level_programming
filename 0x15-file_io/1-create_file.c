71feda6a3d89e10119b9dfb6286b285341364775
bravin-atonya <bravinatonya.brani@gmail.com>
2022-11-04 12:53:29 -0700

    correct betty styling

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -45,10 +45,7 @@ int create_file(const char *filename, char *text_content)
 	access_granted = open(filename, O_CREAT | O_RDWR | O_APPEND, 00600);
 
 	if (access_granted == -1)
-	{
-		printf("i faild to create the file");
 		return (-1);
-	}
 
 	access_granted = write(access_granted, text_content, _strlen(text_content));
 
