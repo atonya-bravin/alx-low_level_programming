abe4fcd02eefd7e4b55350dff8332b0aba608e82
bravin-atonya <bravinatonya.brani@gmail.com>
2022-11-21 11:49:10 -0800

    done it all over again

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -10,8 +10,7 @@
 int main(void)
 {
 	char *concat;
-
-	concat = string_nconcat("Best ", NULL, 6);
+	concat = string_nconcat(NULL, "School !!!", 6);
 	printf("%s\n", concat);
 	free(concat);
 	return (0);
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,42 +12,46 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int first_string_len = 0;
-	int second_string_len = 0;
-	int number_of_bytes = n;
-	char *new_string_pointer;
-	int char_counter = 0;
-	int total_string_len;
-	int sec_string_char_counter = 0;
+	char *concatinated_string;
+	unsigned int first_string_length = 0;
+	unsigned int second_string_length = 0;
+	unsigned int total_strings_length;
+	unsigned int char_counter = 0;
+	int s2_char_counter = 0;
+	int subset_string_counter = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[first_string_len] != '\0')
-		first_string_len++;
-
-	while (s2[second_string_len] != '\0')
-		second_string_len++;
-
-	if (number_of_bytes >= second_string_len)
-		total_string_len = first_string_len + 1 + second_string_len;
+	while(s1[subset_string_counter] != '\0')
+	{
+		subset_string_counter++;
+		first_string_length++;
+	}
+	subset_string_counter = 0;
+	while(s2[subset_string_counter] != '\0')
+	{
+		subset_string_counter++;
+		second_string_length++;
+	}
+	if (n >= second_string_length)
+		total_strings_length = first_string_length + second_string_length;
 	else
-		total_string_len = first_string_len + 1 + number_of_bytes;
-	new_string_pointer = malloc(sizeof(char) * total_string_len);
-	if (new_string_pointer == NULL)
+		total_strings_length = first_string_length + n;
+	concatinated_string = malloc(sizeof(char) * total_strings_length);
+	if (concatinated_string == NULL)
 		return (NULL);
-	while (char_counter < first_string_len)
+	while(char_counter < first_string_length)
 	{
-		new_string_pointer[char_counter] = s1[char_counter];
+		concatinated_string[char_counter] = s1[char_counter];
 		char_counter++;
 	}
-	while (char_counter < total_string_len - 1)
+	while(char_counter < total_strings_length)
 	{
-		new_string_pointer[char_counter] = s2[sec_string_char_counter];
+		concatinated_string[char_counter] = s2[s2_char_counter];
 		char_counter++;
-		sec_string_char_counter++;
+		s2_char_counter++;
 	}
-	new_string_pointer[total_string_len] = '\0';
-	return (new_string_pointer);
+	return(concatinated_string);
 }
