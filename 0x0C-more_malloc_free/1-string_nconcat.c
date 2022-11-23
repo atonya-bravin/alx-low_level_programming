ee6faaac28f4c653002e2f6813ee1a7212b30ced
bravin-atonya <bravinatonya.brani@gmail.com>
2022-11-23 11:13:01 -0800

    formulate _strlen function to calculate string length

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,6 +2,26 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - finds the length of the passed string
+ *
+ * @string: target string
+ *
+ * Return: Always string_length
+ */
+size_t _strlen(char *string)
+{
+	unsigned int string_length = 0;
+	int subset_string_counter = 0;
+
+	while (string[subset_string_counter] != '\0')
+	{
+		subset_string_counter++;
+		string_length++;
+	}
+	return (string_length);
+}
+
+/**
  * string_nconcat - concatenates two strings
  *
  * @s1: first string
@@ -18,23 +38,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int total_strings_length;
 	unsigned int char_counter = 0;
 	int s2_char_counter = 0;
-	int subset_string_counter = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while(s1[subset_string_counter] != '\0')
-	{
-		subset_string_counter++;
-		first_string_length++;
-	}
-	subset_string_counter = 0;
-	while(s2[subset_string_counter] != '\0')
-	{
-		subset_string_counter++;
-		second_string_length++;
-	}
+
+	first_string_length = _strlen(s1);
+	second_string_length = _strlen(s2);
+
 	if (n >= second_string_length)
 		total_strings_length = first_string_length + second_string_length;
 	else
@@ -42,16 +54,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	concatinated_string = malloc(sizeof(char) * total_strings_length);
 	if (concatinated_string == NULL)
 		return (NULL);
-	while(char_counter < first_string_length)
+	while (char_counter < first_string_length)
 	{
 		concatinated_string[char_counter] = s1[char_counter];
 		char_counter++;
 	}
-	while(char_counter < total_strings_length)
+	while (char_counter < total_strings_length)
 	{
 		concatinated_string[char_counter] = s2[s2_char_counter];
 		char_counter++;
 		s2_char_counter++;
 	}
-	return(concatinated_string);
+	return (concatinated_string);
 }
