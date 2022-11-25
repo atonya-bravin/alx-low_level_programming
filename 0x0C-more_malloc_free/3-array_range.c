62ecef7f56811e8f8dbf66e7f76a18815982b31a
bravin-atonya <bravinatonya.brani@gmail.com>
2022-11-25 11:34:18 -0800

    creation of integer array and insertion of numbers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integes
+ *
+ * @min: minimum value in the integer array
+ * @max: maximum value in the integer array
+ *
+ * Return: always pointer to integer array
+ *
+ */
+
+int *array_range(int min, int max)
+{
+	int array_size;
+	int *integer_array;
+	int index = 0;
+
+	if (min > max)
+		return (NULL);
+	else
+	{
+		array_size = max - min;
+		integer_array = malloc(sizeof(int) * array_size);
+		if (integer_array == NULL)
+			return (NULL);
+		else
+		{
+			while (min <= max)
+			{
+				integer_array[index] = min;
+				min++;
+			}
+		}
+	}
+	return (integer_array);
+}
