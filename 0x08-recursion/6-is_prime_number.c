f4b8ddec0283868f05e5f1559d767461f54abcd6
bravin-atonya <bravinatonya.brani@gmail.com>
2022-11-27 11:13:44 -0800

    find prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,3 +1,8 @@
+#include "main.h"
+#include <stdlib.h>
+
+int determine_prime(int number, int counter);
+
 /**
  * is_prime_number - finds prime numbers
  *
@@ -8,17 +13,29 @@
 
 int is_prime_number(int n)
 {
-	int counter = 1;
-	int factors_counter = 0;
 	int number = n;
 
-	while (counter <= number)
-	{
-		if ((number % counter) == 0)
-			factors_counter++;
-		counter++;
-	}
-	if (factors_counter == 2)
+	return (determine_prime(number, 1));
+}
+
+/**
+ * determine_prime - determines if the number is a prime number
+ *
+ * @number: the number we are testing
+ * @counter: the iterating number
+ *
+ * Return: 1(success) or 0(fail)
+ */
+int determine_prime(int number, int counter)
+{
+	if (number <= 1)
+		return (0);
+
+	if (number % counter == 0 && counter > 1)
+		return (0);
+
+	if ((number / counter) < counter)
 		return (1);
-	return (0);
+
+	return (determine_prime(number, counter + 1));
 }
