9a4e0e73f39696d9b25ee7f6c2ef6afa967ef6a8
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-08 10:24:22 -0800

    0-print_dlistint.c:correction on styling

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,17 +1,26 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * print_dlistint - counts and returns the number of elements in a list while
+ * printing them
+ *
+ * @h: the head to the list
+ *
+ * Return: number of nodes in the list
+ */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t number_of_nodes = 0;
+
 	if (h == NULL)
-		return -1;
+		return (0);
 	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-	       	number_of_nodes++;
+		number_of_nodes++;
 	}
 	printf("%d\n", h->n);
 	return (number_of_nodes + 1);
