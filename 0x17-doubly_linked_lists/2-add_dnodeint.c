3c1fc3db63073e884d23061d2cc6f030be2d0262
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-09 18:38:47 -0800

    2-add_dnodeint.c: correction on styling

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,17 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * add_dnodeint - creates  a doubly linked list and adds elements to beginning
+ *
+ * @head: the head of the linked list
+ * @n: the value of the created node
+ *
+ * Return: the head of the linked list
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	dlistint_t *current_node;
+	dlistint_t *new_node = NULL;
+	dlistint_t *current_node = NULL;
 
 	if (*head)
 	{
 		if ((*head)->prev != NULL)
+		{
 			current_node = *head;
+			new_node = malloc(sizeof(dlistint_t));
+			if (new_node == NULL)
+				return (NULL);
+		}
 		while (current_node->prev != NULL)
-			current_node = current_node->prev;	
+			current_node = current_node->prev;
 		new_node->n = n;
 		new_node->prev = NULL;
 		new_node->next = current_node;
@@ -22,6 +35,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	else
 	{
 		*head = malloc(sizeof(dlistint_t));
+		if (*head == NULL)
+			return (NULL);
 		current_node = *head;
 		(*head)->next = NULL;
 		(*head)->prev = NULL;
