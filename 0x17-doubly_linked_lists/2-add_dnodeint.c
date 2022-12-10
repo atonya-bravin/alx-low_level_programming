296fa36d8803ccbc3bf66de44fedb91bc14ded6b
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-09 18:48:35 -0800

    2-add_dnodeint.c: correction on styling

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,15 +12,14 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = NULL;
-	dlistint_t *current_node = NULL;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *current_node;
 
 	if (*head)
 	{
 		if ((*head)->prev != NULL)
 		{
 			current_node = *head;
-			new_node = malloc(sizeof(dlistint_t));
 			if (new_node == NULL)
 				return (NULL);
 		}
