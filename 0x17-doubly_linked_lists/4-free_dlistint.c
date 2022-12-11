cc14f0950e9bcb10df4e130a6ee097de939e9f42
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-10 23:09:21 -0800

    4-free_dlistint.c: remove from left to right

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,15 +1,22 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * free_dlistint - frees a linked list starting from the head from left to right
+ *
+ * @head: the head of the linked list
+ *
+ * Return: 
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *current_node = head;
-	dlistint_t *to_free_node;
+	dlistint_t *next;
 
 	while (current_node != NULL)
 	{
 		next = current_node->next;
-		free(current);
-		current = next;
+		free(current_node);
+		current_node = next;
 	}
 }
