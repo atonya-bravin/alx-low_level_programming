7ca52d8d64d9305259ea0fe1d811c6000c652989
bravin-atonya <bravinatonya.brani@gmail.com>
2023-01-06 19:20:51 -0800

    0-hash_table_create.c: creation of an empty hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,17 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 
+/**
+ * hash_table_create - creates an empty hash table
+ *
+ * @size: the size of the hash table
+ *
+ * Return: the newly created hash table
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *my_hash_table = malloc(size);
 
-	return my_hash_table;
+	return (my_hash_table);
 }
