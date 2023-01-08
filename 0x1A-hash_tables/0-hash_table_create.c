52e09b9bf194c26af0c0d45cad7706c2c892dca6
bravin-atonya <bravinatonya.brani@gmail.com>
2023-01-08 12:29:51 -0800

    0-hash_table_create.c: create a new hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,6 +18,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (my_htable == NULL)
 		return (NULL);
 	htable_array = malloc(sizeof(hash_node_t *) * size);
+	if (htable_array == NULL)
+		return (NULL);
 
 	for (index = 0; index < size; index++)
 	{
