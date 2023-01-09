2fade2d50d2c518d5258364c3cf634c6ee4f420e
bravin-atonya <bravinatonya.brani@gmail.com>
2023-01-08 18:51:09 -0800

    4-hash_table_get.c: retrieves a value associated with a key.

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_get - retrives the value of a given key
+ *
+ * @ht: the hash table to work on
+ * @key: the key of the value to be retrived
+ *
+ * Return: the value of the key
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
+		return (NULL);
+	index = key_index((unsigned char *)key, ht->size);
+
+	return ((ht->array[index])->value);
+}
