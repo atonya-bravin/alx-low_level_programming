6a8593328d15a4f3bf735d05e37aacb23c223728
bravin-atonya <bravinatonya.brani@gmail.com>
2023-01-12 05:21:44 -0800

    5-hash_table_print.c: print a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - prints a hash table.
+ *
+ * @ht: the hash table to be printed
+ *
+ * Return: Nothing
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *ht_element;
+	unsigned long int index = 0;
+	int not_null_elements = 0;
+
+	printf("{");
+	if (ht != NULL)
+	{
+		for (index = 0; index < ht->size; index++)
+		{
+			ht_element = ht->array[index];
+			if (ht_element)
+			{
+				if (not_null_elements != 0)
+					printf(",");
+				printf("'%s':", (char *)ht_element->key);
+				printf("'%s'", (char *)ht_element->value);
+				not_null_elements++;
+			}
+		}
+	}
+	printf("}\n");
+}
