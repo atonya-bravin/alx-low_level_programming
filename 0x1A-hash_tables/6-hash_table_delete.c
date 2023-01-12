60e5c1db021c3b1ff18ac839efc7388b487e7ccf
bravin-atonya <bravinatonya.brani@gmail.com>
2023-01-12 06:04:47 -0800

    -hash_table_delete.c: deletes a hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * hash_table_print - prints a hash table.
+ *
+ * @ht: the hash table to be printed
+ *
+ * Return: Nothing
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *ht_element, *next_node;
+	unsigned long int index = 0;
+
+	if (ht == NULL)
+		return;
+
+	for (index = 0; index < ht->size; index++)
+	{
+		ht_element = ht->array[index];
+		while (ht_element)
+		{
+			next_node = ht_element->next;
+			free(ht_element);
+			ht_element = next_node;
+		}
+	}
+}
