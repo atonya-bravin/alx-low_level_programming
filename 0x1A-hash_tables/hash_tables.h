67084de2465d3e334f12bcb3fc09e20ec5599a11
bravin-atonya <bravinatonya.brani@gmail.com>
2023-01-12 03:29:00 -0800

    0-hash_table_create.c: style correction

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -2,13 +2,13 @@
 #define HASH_TABLE
 
 /**
- *  * struct hash_node_s - Node of a hash table
- *   *
- *    * @key: The key, string
- *     * The key is unique in the HashTable
- *      * @value: The value corresponding to a key
- *       * @next: A pointer to the next node of the List
- *        */
+ * struct hash_node_s - Node of a hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
 typedef struct hash_node_s
 {
 	char *key;
@@ -17,13 +17,13 @@ typedef struct hash_node_s
 } hash_node_t;
 
 /**
- *  * struct hash_table_s - Hash table data structure
- *   *
- *    * @size: The size of the array
- *     * @array: An array of size @size
- *      * Each cell of this array is a pointer to the first node of a linked list,
- *       * because we want our HashTable to use a Chaining collision handling
- *        */
+ * struct hash_table_s - Hash table data structure
+ *
+ * @size: The size of the array
+ * @array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list,
+ * because we want our HashTable to use a Chaining collision handling
+ */
 typedef struct hash_table_s
 {
 	unsigned long int size;
@@ -34,4 +34,4 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
-#endif 
+#endif
