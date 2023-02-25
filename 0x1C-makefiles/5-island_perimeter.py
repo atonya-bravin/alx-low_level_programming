f87e4bcb173336edc2a478a051faaae248534597
bravin-atonya <bravinatonya.brani@gmail.com>
2023-02-25 09:14:14 -0800

    pycodestyle compliance

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,22 +7,17 @@ def island_perimeter(grid):
         island_perimeter: calfulates the perimeter of an island
     """
 
-    perimeter = 0;
+    perimeter = 0
     for outter_index in range(len(grid)):
         for inner_index in range(len(grid[outter_index])):
-                if grid[outter_index][inner_index] == 1 and inner_index > 0:
-                    if grid[outter_index-1][inner_index] == 0:
-                        perimeter = perimeter + 1
-                    if grid[outter_index][inner_index-1] == 0:
-                        perimeter = perimeter + 1
-                    if grid[outter_index][inner_index+1] == 0:
-                        perimeter = perimeter + 1
-                    if grid[outter_index+1][inner_index] == 0:
-                        perimeter = perimeter + 1
+            if grid[outter_index][inner_index] == 1 and inner_index > 0:
+                if grid[outter_index-1][inner_index] == 0:
+                    perimeter = perimeter + 1
+                if grid[outter_index][inner_index-1] == 0:
+                    perimeter = perimeter + 1
+                if grid[outter_index][inner_index+1] == 0:
+                    perimeter = perimeter + 1
+                if grid[outter_index+1][inner_index] == 0:
+                    perimeter = perimeter + 1
 
     return perimeter
-
-                    
-
-
-
