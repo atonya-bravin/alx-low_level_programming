4b1864cad201c1590f46a49741df09d73a34902d
bravin-atonya <bravinatonya.brani@gmail.com>
2023-03-10 22:23:23 -0800

    adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,7 @@
 #include "lists.h"
+#include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * add_node -  adds a new node at the beginning of our list
@@ -13,44 +15,27 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newNode = malloc(sizeof(list_t));
+	list_t *new_node;
 	unsigned int string_length = 0;
 	int char_ctr = 0;
 
-	if (newNode == NULL)
-	{
-		free(newNode);
-		return (NULL);
-	}
 	for (char_ctr = 0; str != NULL && str[char_ctr] != '\0'; char_ctr++)
-	{
 		string_length++;
-	}
-	if (*head == NULL)
+
+	if (head != NULL && str != NULL)
 	{
-		*head = malloc(sizeof(list_t));
-		if (head == NULL)
-		{
-			free(head);
+		new_node = malloc(sizeof(list_t));
+		if (new_node == NULL)
 			return (NULL);
-		}
-		(*head)->str = strdup(str);
-		(*head)->len = string_length;
+
+		new_node->str = strdup(str);
+		if (new_node->str == NULL)
+			return (NULL);
+		new_node->len = string_length;
+		new_node->next = *head;
+
+		*head = new_node;
 	}
-	else
-	{
-		if (str == NULL)
-		{
-			newNode->str = "(nil)";
-			newNode->len = 0;
-		}
-		else
-		{
-			newNode->str = strdup(str);
-			newNode->len = string_length;
-		}
-		newNode->next = *head;
-		*head = newNode;
-	}
-	return (*head);
+
+	return (new_node);
 }
