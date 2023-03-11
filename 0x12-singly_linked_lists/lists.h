4b7cdec0e12350d2ff1589f1a720d3ab97d7835a
bravin-atonya <bravinatonya.brani@gmail.com>
2023-03-11 00:01:51 -0800

    inclusion of the get_string_len function declaration

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,5 +24,5 @@ size_t list_len(const list_t *h);
 void free_list(list_t *head);
 list_t *add_node_end(list_t **head, const char *str);
 list_t *add_node(list_t **head, const char *str);
-
+size_t get_string_len(const char *str);
 #endif
