7d0415ae2d5d1e58139f8f00f2ef98800b1ba4d1
bravin-atonya <bravinatonya.brani@gmail.com>
2023-03-10 23:50:00 -0800

    adds a new node at the end of a list_t list.

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,15 +13,60 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *temp_node;
+	list_t *new_node, *link;
+	unsigned int string_length = 0;
 
-	if(head == NULL)
-		head = malloc(sizeof(list_t));
-	else
+	string_length = get_string_len(str);
+
+	if (str != NULL)
 	{
-		temp_node->next = malloc(sizeof(list_t));
-		temp_node->str = str;
-		head = temp_node;
+		new_node = malloc(sizeof(list_t));
+		if (new_node == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->next = NULL;
+		new_node->str = strdup(str);
+		if (new_node->str == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->len = string_length;
+		if (*head == NULL)
+		{
+			*head = new_node;
+			return (*head);
+		}
+		link = *head;
+		while (link != NULL)
+		{
+			if (link->next == NULL)
+			{
+				link->next = new_node;
+				break;
+			}
+			link = link->next;
+		}
 	}
-	return (*head);
+	return (new_node);
+}
+
+/**
+ * get_string_len - finds the length of a string
+ * @str: string to be used
+ * Return: string length
+ */
+
+size_t get_string_len(const char *str)
+{
+	unsigned int string_length = 0;
+	int char_ctr = 0;
+
+	for (char_ctr = 0; str != NULL && str[char_ctr] != '\0'; char_ctr++)
+	{
+		string_length++;
+	}
+	return (string_length);
 }
