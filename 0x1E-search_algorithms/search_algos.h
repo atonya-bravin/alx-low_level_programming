275023fb3b2de872178aafd99244ba8352a296aa
bravin-atonya <bravinatonya.brani@gmail.com>
2023-04-11 16:33:35 +0000

    inclusion of the binary_search declaration

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef SEARCH_ALGORITHMS
+#define SEARCH_ALGORITHMS
+#include <stddef.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
